cbc1dee494c5d1f57b6375d30e67a2de19e9eca4
Talgat Shukataev <tshukataev@mail.ru>
2023-02-20 20:25:35 +0500

    Корректировка

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,4 +1,4 @@
-const sliderArray = [...document.querySelectorAll(".slider__item")];
+const slider = [...document.querySelectorAll(".slider__item")];
 const next = document.querySelector(".slider__arrow_next");
 const prev = document.querySelector(".slider__arrow_prev");
 
@@ -6,7 +6,7 @@ next.onclick = nextSlide;
 prev.onclick = prevSlide;
 
 function getCurrentIndex() {
-  let currentIndex = sliderArray.findIndex((item) =>
+  let currentIndex = slider.findIndex((item) =>
     item.closest(".slider__item_active")
   );
   return currentIndex;
@@ -15,8 +15,8 @@ function getCurrentIndex() {
 function nextSlide() {
   let currentIndex = getCurrentIndex();
   if (currentIndex !== -1) {
-    sliderArray[currentIndex].classList.toggle("slider__item_active");    
-    if (currentIndex === sliderArray.length - 1) {
+    slider[currentIndex].classList.toggle("slider__item_active");
+    if (currentIndex === slider.length - 1) {
       currentIndex = 0;
     } else {
       currentIndex++;
@@ -24,20 +24,20 @@ function nextSlide() {
   } else {
     alert("Ошибка");
   }
-  sliderArray[currentIndex].classList.toggle("slider__item_active");
+  slider[currentIndex].classList.toggle("slider__item_active");
 }
 
 function prevSlide() {
   let currentIndex = getCurrentIndex();
   if (currentIndex !== -1) {
-    sliderArray[currentIndex].classList.toggle("slider__item_active");    
+    slider[currentIndex].classList.toggle("slider__item_active");
     if (currentIndex === 0) {
-      currentIndex = sliderArray.length - 1;
+      currentIndex = slider.length - 1;
     } else {
       currentIndex--;
     }
   } else {
     alert("Ошибка");
   }
-  sliderArray[currentIndex].classList.toggle("slider__item_active");  
+  slider[currentIndex].classList.toggle("slider__item_active");
 }
