6d2c8a5351510d6cbd7dfbdc0dabda390fa5fe76
Talgat Shukataev <tshukataev@mail.ru>
2023-02-23 22:36:08 +0500

    Корректировка 2ой задачи

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,13 +1,23 @@
 const menu = [...document.querySelectorAll(".menu__item")];
-const menuSub = document.querySelector(".menu_sub")
-for (let i = 0; i < menu.length; i++) {
-  menu[i].onclick = function () {
-    if (menu[i].classList.toggle(".menu_sub")) {
-      menuSub.classList.add("menu_active")
-      return false;
-    } else {
-      menuSub.classList.remove("menu_active")
-      return false;
+//const menuSub = document.querySelectorall(".menu_sub");
+menu.forEach((element) => {
+  let menuSub = element.closest(".menu__item").querySelector(".menu_sub");
+  if (!menuSub) {
+    return;
+  } else {
+    element.onclick = function () {
+      menuSub.classList.toggle("menu_active");
     }
   }
-}
+})
+//for (let i = 0; i < menu.length; i++) {
+//  let menuList = menu[i].closest(".menu__item");
+//  let menuSub = menuList.querySelector(".menu_sub");  
+//  if (!menuSub) {
+//    return false;
+//  } else {
+//    menu[i].onclick = function () {
+//      menuSub.classList.toggle("menu_active");
+//    }
+//  }
+//}
