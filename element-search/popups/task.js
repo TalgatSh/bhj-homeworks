2ca59618770035bc2e4608b24534a6469c98ba15
Talgat Shukataev <tshukataev@mail.ru>
2023-02-23 15:48:19 +0500

    Корректировка 1го задания

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,16 +1,21 @@
 const modal = document.getElementById("modal_main");
-const modalClose = document.querySelector(".modal__close");
+const modalClose = [...document.querySelectorAll(".modal__close")];
 const modalShow = document.querySelector(".show-success");
 const modalSuccess = document.getElementById("modal_success");
 
 modal.classList.add("modal_active");
-modalClose.onclick = function() {
-  modal.classList.remove("modal_active");
+for (let i = 0; i < modalClose.length; i++) {
+  modalClose[i].onclick = function() {
+    let parentModal = this.closest(".modal");
+    parentModal.classList.remove("modal_active");
+  }
 }
+
 modalShow.onclick = function() {
   modalSuccess.classList.add("modal_active");
+  modal.classList.remove("modal_active");
 }
 modalSuccess.onclick = function() {
   modal.classList.remove("modal_active");
-  modalSuccess.classList.remove("modal_active");
 }
+
