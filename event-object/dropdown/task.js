fa8f360d2b39a417a2b1b5fe61126524c30dfe6a
Talgat Shukataev <tshukataev@mail.ru>
2023-03-20 17:15:42 +0500

    Домашнее задание, 1 задача

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,13 +1,14 @@
 const menu = document.querySelector(".dropdown__list");
 const btnMenu = document.querySelector(".dropdown__value");
 const nameMenu = [...document.querySelectorAll(".dropdown__link")];
-btnMenu.addEventListener("click", () => {
-  menu.classList.toggle("dropdown__list_active");
-  if (menu.classList.contains("dropdown__list_active")) {
+btnMenu.addEventListener("click", () => {  
+  if (!menu.classList.contains("dropdown__list_active")) {
+    menu.classList.toggle("dropdown__list_active");
     nameMenu.forEach((e) => {
       e.addEventListener("click", (event) => {
       btnMenu.textContent = e.textContent;
       event.preventDefault();
+      menu.classList.remove("dropdown__list_active");
       })
     })
   }
