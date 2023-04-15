dc31d349231add6a9bc9bdbc1b8b228c05081bce
Talgat Shukataev <tshukataev@mail.ru>
2023-04-15 23:00:12 +0500

    Домашнее задание готово

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -10,13 +10,14 @@ parent.addEventListener("click", (event) => {
   let control = event.target;
   let size = control.dataset.size || '';
   event.preventDefault();
-  if (control.classList.contains("font-size") && book.classList.contains("book")) {
+  if (control.classList.contains("font-size") && book.className == "book") {
     for(let i = 0; i < buttons.length; i++) {
       buttons[i].classList.remove("font-size_active");
-      book.classList.remove(`book_fs-${size}`);
+      book.className = (`book book_fs-${size}`);
     }
     control.classList.add("font-size_active");
-    book.classList.add(`book_fs-${size}`);
+  } else {
+      book.className = `book`;
   }
 });
 
